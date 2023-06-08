0dd42ee8ce866fd92b1a64fb27037ef2448fe705
ÄntonPakhorukov <pakhorukov.a@mail.ru>
2023-06-08 23:34:13 +0700

    Home_work in process

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        BinaryTree<Integer> tree = new BinaryTree<>();
+        tree.add(5);
+        tree.add(2);
+        tree.add(7);
+        tree.add(1);
+        tree.print();
+    }
+}
